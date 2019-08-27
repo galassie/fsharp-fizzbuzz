f1f6fea6df97a827b67426dc8f7bb3f60476d7bd
Enrico Galassi <enrico.galassi.88@gmail.com>
2019-08-28 00:16:13 +0200

    Add console fsproj with example of printing first 100 numbers

---
+++ src/FSharp.FizzBuzz.Console/Program.fs
@@ -0,0 +1,10 @@
+﻿open FSharp.FizzBuzz
+
+[<EntryPoint>]
+let main argv =
+    printfn "Generate first 100 numbers:" 
+    seq { 1..100 }
+    |> Seq.map FizzBuzzGenerator.gen
+    |> Seq.iter (printfn "%s")
+    0 // return an integer exit code
+
