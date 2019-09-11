40904e5f7c4e901e56ff70f025afa65691a77b58
Enrico Galassi <enrico.galassi.88@gmail.com>
2019-09-11 22:47:03 +0200

    Replace sequences with Arrays, fix solution visualization in visual studio

--- src/FSharp.FizzBuzz/FizzBuzzGenerator.fs
+++ src/FSharp.FizzBuzz/FizzBuzzGenerator.fs
@@ -1,22 +1,19 @@
 namespace FSharp.FizzBuzz
 
 module FizzBuzzGenerator =
-    let gen (x:int) = 
-        seq {
-          yield (if x % 3 = 0 then "Fizz" else "")
-          yield (if x % 5 = 0 then "Buzz" else "")
-        }
-        |> Seq.reduce ( + )
+    let gen x = 
+        [|
+          (if x % 3 = 0 then "Fizz" else "");
+          (if x % 5 = 0 then "Buzz" else "")
+        |]
+        |> Array.reduce ( + )
         |> function
             | "" -> sprintf "%d" x
             | __ -> __
 
-    let genSimplified (x:int) =
-        let seqString = seq {
-                yield (if x % 3 = 0 then "Fizz" else "")
-                yield (if x % 5 = 0 then "Buzz" else "")
-            }
-        let seqReduced = Seq.reduce (fun acc item -> acc + item) seqString
-        match seqReduced with
+    let genSimplified x =
+        let arrayStrings = [| (if x % 3 = 0 then "Fizz" else ""); (if x % 5 = 0 then "Buzz" else "") |]
+        let arrayReduced = Array.reduce (fun acc item -> acc + item) arrayStrings
+        match arrayReduced with
         | "" -> sprintf "%d" x
         | __ -> __
